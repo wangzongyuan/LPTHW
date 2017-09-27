e6abd6a44de8687d88672ba80388afbe0cdb029a
Wangzongyuan <wangzongyuanxue@gmail.com>
2017-09-27 20:51:26 +0800

    习题 1: 第一个程序
    习题 1: 第一个程序

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# --coding:utf8--
+print("Hello world")
+print("Hello Again")
+print("I like type this.")
+print("This is fun")
+print('Yay! Printing')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
+print('妳好，我是Python!')
+print('這是我的練習題')
+print('下面要加上#的註釋')
+# This is pound
+# This is hash
+# This is mesh

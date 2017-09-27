4bdda02c478ce861a660af9f7a32676c2a4e68fc
Wangzongyuan <wangzongyuanxue@gmail.com>
2017-09-27 21:27:25 +0800

    习题 3: 数字和数学计算
    数字和数学计算
    学习重点为运算式，以及如何使用操作符。

---
+++ ex3.py
@@ -0,0 +1,34 @@
+# --coding:utf8--
+print('I will now count my chickens:')
+print("Heans"), 25 + 30 / 6 # Heans 是什麼意思？
+print("Roosters"), 100 - 25 * 3 % 4 #這邊我需要幫忙加上一個註釋
+print("Now I will count the eggs:") 
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #這是一個算數式
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7 ) 
+print("What is 3 + 2 ="), 3 + 2 # 這是3+2的解答
+print("What is 5 - 7 ="), 5 - 7 # 這是5-7的解答
+print("That is why it's False.")
+print("How about some more.")
+print("Is it greater?"), 5 > -2
+print("Is it greater or equal?"), 5 >= 2
+print("Is it less or equal?"), 5 <= -2
+
+
+# 以下為練習題
+
+print("BMI的計算公式為體重/身高2次方（公尺2次方）") # 應該還有很多公式可以練習
+print("身高171，體重66公斤的BMI等於多少？"), 66 / (1.71*1.71) # 忘了2次方的用法
+print("2次方練習"),66 / 1.71**2 # a**n 則為 a的幾次方  
+
+# 本章心得，學習計算式。另外在尋找練習算式的時候發現BMI很適合拿來練習算式以及未來的if elas等等的練習題。
+
+# 以下列出運算式
+# 運算符   描敘符
+# +       加法- 對操作符兩側增加值  
+# -       減法- 減去從左側操作數右側操作數 
+# *       乘法- 相乘的運算符兩側的值
+# /       除法- 由右側操作數除以左側操作數
+# %       模__- ??
+# **      次方- 執行對操作指數的計算
+# //      地板除- 

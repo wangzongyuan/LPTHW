414dbbc450faf31b2e61edbd03f91fd7dd80cf49
Wangzongyuan <wangzongyuanxue@gmail.com>
2017-09-28 20:55:04 +0800

    习题 7: 更多打印
    更多打印
    本章节主要练习字串与Print显示的练习，增加熟练度

---
+++ ex7.py
@@ -0,0 +1,44 @@
+# --coding:utf8--
+
+print("Mary had a little lamb.") # 使用print函數，顯示字串String
+print("Its fleece was white as %s." % 'snow') # 使用print函數，顯示字串String
+print("And everywhere that Mary went.") # 使用print函數，顯示字串String 
+print("." * 10) # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch a comma at the end. try  removing it to see what happens. 
+
+print(end1 + end2 + end3 + end4 + end5 +end6) # 列印end系列變數代表的字母
+print(end7 + end8 + end9 + end10 + end11 + end12) # 列印end系列變數代表的字母
+
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12)
+print(end1 + end2 + end3 + end4 + end5 + end6 , end7 + end8 + end9 + end10 + end11 + end12) 
+# 上面在end6與end7之間加上,看看有什麼不同
+
+print(end1 + end2 + end3 + end4 + end5 + end6 ), (end7 + end8 + end9 + end10 + end11 + end12) 
+# 上面與前一個例子相似，想確認這樣的用法，是否可以顯示出與試題相同答案。
+
+'''
+加分习题
+
+接下来几节的加分习题是一样的。
+1. 逆向阅读，在每一行的上面加一行注解。
+2. 倒着朗读出来，找出自己的错误。
+3. 从现在开始，把你的错误记录下来，写在一张纸上。
+4. 在开始下一节习题时，阅读一遍你记录下来的错误，并且尽量避免在下个练习中再犯同样的错误。
+5. 记住，每个人都会犯错误。程序员和魔术师一样，他们希望大家认为他们从不犯错，不过这只是表象而已，他们每时每刻都在犯错。
+'''
+
+# 本章节主要练习字串与Print显示的练习，增加熟练度

107dbe7963215c69fcef0eeb8ff4449aff574668
Wangzongyuan <wangzongyuanxue@gmail.com>
2017-09-29 10:48:57 -0500

    习题 9: 打印，打印，打印
    打印，打印，打印
    本章的重点为print的练习，以及学习\n的用法

---
+++ ex9.py
@@ -0,0 +1,50 @@
+# --coding:utf8--
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFab\nMar\nApr\nMay\nJun\nJul\nAug"
+print("Here are the days:"), days
+print("Here are the months:"), months
+
+print("""
+	There's something going on here.
+	With the three double-quotes.
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, 5, or 6.
+	""") 
+'''
+第一次練習的結果為：
+('Here are the days:', 'Mon Tue Wed Thu Fri Sat Sun')
+('Here are the months:', 'Jan\nFab\nMar\nApr\nMay\nJun\nJul\nAug')
+
+	There's something going on here.
+	With the three double-quotes.
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, 5, or 6.
+上面多出了()
+決定再進行一次練習，將Print原先的()進行縮小，在,前完成（）閉合。看看結果為何？
+顯示結果為下：
+Here are the days: Mon Tue Wed Thu Fri Sat Sun
+Here are the months: Jan
+Fab
+Mar
+Apr
+May
+Jun
+Jul
+Aug
+
+	There's something going on here.
+	With the three double-quotes.
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, 5, or 6.
+
+'''
+'''
+加分习题
+
+1. 自己检查结果，记录你犯过的错误，并且在下个练习中尽量不犯同样的错误。
+
+'''
+
+# 另外本章節中使用\ 。 \的意義為換行。\是轉義的意思,'\n'為換行, '\t'為tab , '\\'為\
